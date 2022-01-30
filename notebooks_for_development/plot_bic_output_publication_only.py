e2831d9250902b4332764382a84f700941f9c73e
Eckhart Spalding <espaldin@nd.edu>
2022-01-30 09:34:23 -0500

    made final bic plot for paper

--- notebooks_for_development/plot_bic_output_publication_only.py
+++ notebooks_for_development/plot_bic_output_publication_only.py
@@ -16,13 +16,13 @@ from typing import *
 
 
 
-input_file_name = "bic_run_20220129/results_20220129.csv"
+input_file_name = "bic_run_20220129_3_weighted_squares/results_bic_run_20220129_3_weighted_squares.csv"
 output_plot_name = "junk.pdf"
 
 df = pd.read_csv(input_file_name,names=["coeffs_array",
                                                     "bic",
                                                     "n_params",
-                                                    "ssr",
+                                                    "chi_squared",
                                                     "n_samples",
                                                     "a_coeff",
                                                     "b_coeff",
@@ -36,7 +36,7 @@ df = pd.read_csv(input_file_name,names=["coeffs_array",
                                                     "n_coeff",
                                                     "res"],delimiter=";")
 
-BIC_baseline = 296.30844271382693 # from Layden a,b,c,d model
+BIC_baseline = 458.1620541985783 # from Layden a,b,c,d model
 
 # sort by BIC
 df_sorted = df.sort_values("bic").reset_index()
@@ -71,6 +71,7 @@ ax = df_sorted["del_bic"].plot(kind='barh')
 ax.set_ylabel("Coefficient permutation", fontsize=14)
 ax.set_xlabel("$\Delta$BIC", fontsize=14)
 ax.set_yticklabels(char_string_list_post, fontsize=7)
+ax.set_xlim([-100,300])
 
 # stagger the tick marks
 for tick in ax.yaxis.get_major_ticks()[1::2]:
@@ -82,4 +83,20 @@ for tick in ax.yaxis.get_major_ticks()[1::2]:
 plt.tight_layout()
 plt.savefig(output_plot_name)
 print("Saved BIC plot as " + output_plot_name)
+
+# a second plot, to just look at models that fit better than Layden 1994
+plt.clf()
+ax = df_sorted["del_bic"][:22].plot(kind='barh')
+ax.set_ylabel("Coefficient permutation", fontsize=14)
+ax.set_xlabel("$\Delta$BIC", fontsize=14)
+ax.set_yticklabels(char_string_list_post[:22], fontsize=12)
+ax.set_xlim([-320,10])
+# stagger the tick marks
+for tick in ax.yaxis.get_major_ticks()[1::2]:
+    tick.set_pad(30)
+#for p in ax.patches:
+#    ax.annotate(str(df_sorted["coeffs_array"][p]), (p.get_x() * 1.0, p.get_height() * 1.005))
+plt.tight_layout()
+plt.savefig("junk2.pdf", bbox_inches='tight')
+print("Saved BIC plot as " + "junk2.pdf")
 #plt.show()
